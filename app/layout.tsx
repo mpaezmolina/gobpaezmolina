862e736c464c0f04ccda4730ff0e5c839ca84e86
matias <team@autoapps.ai>
2025-08-26 15:35:31 -0300

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Gobernador Justo Páez Molina",
+  title: "Gob. Páez Molina",
   description: "Gobernador Justo Páez Molina. Nació en El Salto, el 7 de agosto de 1902. Político radical argentino y 52º Gobernador de Córdoba.",
 };
 
