13d1df5492faeda73516fec1d18f7ff899baf836
matias <team@autoapps.ai>
2025-08-26 16:56:02 -0300

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: "Gob. Páez Molina",
   description: "Gobernador Justo Páez Molina. Nació en El Salto, el 7 de agosto de 1902. Político radical argentino y 52º Gobernador de Córdoba.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({children,}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
+      <GoogleAnalytics gaId="G-5J3QZWPNYX" />
     </html>
-  );
+  )
 }
